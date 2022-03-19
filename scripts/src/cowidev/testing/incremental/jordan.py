7fde0a119599d9208abcbf325b797a9ec5cf376c
Rouein <rouein.fani@gmail.com>
2022-03-19 22:49:25 +0300

    fix(test, jordan): new payload and query

--- scripts/src/cowidev/testing/incremental/jordan.py
+++ scripts/src/cowidev/testing/incremental/jordan.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 
 from cowidev.utils import clean_count
-from cowidev.utils.clean.dates import localdate
+from cowidev.utils.clean.dates import localdate, clean_date
 from cowidev.testing.utils.base import CountryTestBase
 
 
@@ -12,7 +12,7 @@ class Jordan(CountryTestBase):
     location: str = "Jordan"
     units: str = "tests performed"
     source_label: str = "Ministry of Health"
-    date: str = localdate("Asia/Amman")
+    week: str = localdate("Asia/Amman", as_datetime=True).isocalendar().week
     notes: str = ""
     source_url: str = (
         "https://wabi-west-europe-d-primary-api.analysis.windows.net/public/reports/querydata?synchronous=true"
@@ -21,14 +21,11 @@ class Jordan(CountryTestBase):
 
     def read(self) -> pd.DataFrame:
         """Reads the data from the source"""
-        data = self.data_body
         try:
-            count = json.loads(requests.post(self.source_url, headers=self.headers, data=json.dumps(data)).content)[
-                "results"
-            ][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"][0]["M0"]
+            count = self._request()
             return self._df_builder(count)
         except KeyError:
-            raise KeyError("No value found. Please check the date.")
+            raise KeyError("No value found. Please modify the payload and headers.")
 
     @property
     def headers(Self):
@@ -37,7 +34,7 @@ class Jordan(CountryTestBase):
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0",
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "en-US",
-            "X-PowerBI-ResourceKey": "1f83a65b-cea7-48df-8be5-85840c51f971",
+            "X-PowerBI-ResourceKey": "f29483dd-2cd3-4be1-9fbd-6c67f0ca1037",
             "Content-Type": "application/json;charset=UTF-8",
             "Origin": "https://app.powerbi.com",
             "Referer": "https://app.powerbi.com/",
@@ -45,8 +42,7 @@ class Jordan(CountryTestBase):
             "Cache-Control": "no-cache",
         }
 
-    @property
-    def data_body(self):
+    def payload(self, week: str = None) -> dict:
         """Request payload"""
         data = {
             "version": "1.0.0",
@@ -58,19 +54,21 @@ class Jordan(CountryTestBase):
                                 "SemanticQueryDataShapeCommand": {
                                     "Query": {
                                         "Version": 2,
-                                        "From": [{"Name": "m", "Entity": "Main dashboard", "Type": 0}],
+                                        "From": [
+                                            {"Name": "w", "Entity": "weekly data", "Type": 0},
+                                        ],
                                         "Select": [
                                             {
                                                 "Aggregation": {
                                                     "Expression": {
                                                         "Column": {
-                                                            "Expression": {"SourceRef": {"Source": "m"}},
-                                                            "Property": "Total Tests",
+                                                            "Expression": {"SourceRef": {"Source": "w"}},
+                                                            "Property": "مجموع الفحوصات المخبرية التراكمي",
                                                         }
                                                     },
                                                     "Function": 0,
                                                 },
-                                                "Name": "CountNonNull(Main dashboard.Total Tests)",
+                                                "Name": "Sum(weekly data.مجموع الفحوصات المخبرية التراكمي)",
                                             }
                                         ],
                                         "Where": [
@@ -80,33 +78,50 @@ class Jordan(CountryTestBase):
                                                         "Expressions": [
                                                             {
                                                                 "Column": {
-                                                                    "Expression": {"SourceRef": {"Source": "m"}},
-                                                                    "Property": "Date",
+                                                                    "Expression": {"SourceRef": {"Source": "w"}},
+                                                                    "Property": "week",
                                                                 }
                                                             }
                                                         ],
-                                                        "Values": [
-                                                            [{"Literal": {"Value": f"datetime'{self.date}T00:00:00'"}}]
-                                                        ],
+                                                        "Values": [[{"Literal": {"Value": f"{week}L"}}]],
                                                     }
                                                 }
-                                            }
+                                            },
                                         ],
                                     },
-                                    "Binding": {"Primary": {"Groupings": [{"Projections": [0]}]}, "Version": 1},
                                     "ExecutionMetricsKind": 1,
                                 }
                             }
                         ]
                     },
                     "QueryId": "",
-                    "ApplicationContext": {"DatasetId": "e856a62e-c5b7-4866-bce5-0faf7154e2e5"},
+                    "ApplicationContext": {
+                        "DatasetId": "805d8b47-2e08-46cc-b1cd-7937fe585c59",
+                    },
                 }
             ],
             "cancelQueries": [],
-            "modelId": 1084599,
+            "modelId": 1187812,
         }
         return data
+
+    def _request(self) -> dict:
+        """Requests data from source."""
+        response = json.loads(
+            requests.post(self.source_url, headers=self.headers, data=json.dumps(self.payload(str(self.week)))).content
+        )["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"][0]
+        if "M0" in response.keys():
+            response = response["M0"]
+        else:
+            self.week -= 1
+            response = self._request()
+        return response
+
+    def _week_to_date(self, week: int) -> str:
+        """Converts week to date."""
+        year = localdate("Asia/Amman", as_datetime=True).isocalendar().year
+        date = clean_date(f"{year} {week} +5", "%Y %W +%w")
+        return date
 
     def _df_builder(self, count: str) -> pd.DataFrame:
         """Builds dataframe from the text data"""
@@ -115,7 +130,7 @@ class Jordan(CountryTestBase):
 
     def pipe_date(self, df: pd.DataFrame) -> pd.DataFrame:
         """Pipes date."""
-        return df.assign(Date=self.date)
+        return df.assign(Date=self._week_to_date(self.week))
 
     def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
         """Pipeline for data."""
