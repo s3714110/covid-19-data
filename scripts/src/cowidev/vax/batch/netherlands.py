42faa56cdf01265173c612fe96145c972ab42940
lucasrodes <lucasrodes@users.noreply.github.com>
2022-03-24 15:50:16 +0100

    fix(vax,nld): add novavax

--- scripts/src/cowidev/vax/batch/netherlands.py
+++ scripts/src/cowidev/vax/batch/netherlands.py
@@ -18,6 +18,7 @@ class Netherlands(CountryVaxBase):
         "Pfizer/BioNTech": "Pfizer/BioNTech",
         "Moderna": "Moderna",
         "Johnson&Johnson": "Johnson&Johnson",
+        "NVXD": "Novavax",
     }
 
     def read(self):
