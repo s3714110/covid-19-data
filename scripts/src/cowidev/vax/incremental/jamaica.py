f51521622d257f0d705000f5108628e2b9458805
lucasrodes <lucasrodes@users.noreply.github.com>
2022-04-11 16:19:50 +0200

    fix(vax,jam): number typo

--- scripts/src/cowidev/vax/incremental/jamaica.py
+++ scripts/src/cowidev/vax/incremental/jamaica.py
@@ -3,70 +3,55 @@ import pandas as pd
 from cowidev.utils.clean import clean_count
 from cowidev.utils.clean.dates import localdate
 from cowidev.utils.web.scraping import get_soup
-from cowidev.vax.utils.incremental import enrich_data, increment
+from cowidev.vax.utils.incremental import enrich_data
+from cowidev.vax.utils.base import CountryVaxBase
 
 
-def read(source: str) -> pd.Series:
-    return connect_parse_data(source)
+class Jamaica(CountryVaxBase):
+    location: str = "Jamaica"
+    source_url: str = "https://vaccination.moh.gov.jm"
+    source_url_ref: str = source_url
 
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        return self._parse_data(soup)
 
-def connect_parse_data(source: str) -> pd.Series:
+    def _parse_data(self, soup) -> pd.Series:
+        counters = soup.find_all(class_="elementor-counter-number")
+        assert len(counters) == 6, "New counter in dashboard?"
+        total_vaccinations = clean_count(counters[0]["data-to-value"].replace(".", ""))
+        first_doses = clean_count(counters[1]["data-to-value"])
+        second_doses = clean_count(counters[2]["data-to-value"])
+        unique_doses = clean_count(counters[3]["data-to-value"])
+        booster_shots = clean_count(counters[4]["data-to-value"])
+        immunocompromised_doses = clean_count(counters[5]["data-to-value"])
 
-    soup = get_soup(source)
+        people_vaccinated = first_doses + unique_doses
+        people_fully_vaccinated = second_doses + unique_doses
+        total_boosters = booster_shots + immunocompromised_doses
 
-    counters = soup.find_all(class_="elementor-counter-number")
-    assert len(counters) == 6, "New counter in dashboard?"
+        date = localdate("America/Jamaica")
 
-    total_vaccinations = clean_count(counters[0]["data-to-value"])
-    first_doses = clean_count(counters[1]["data-to-value"])
-    second_doses = clean_count(counters[2]["data-to-value"])
-    unique_doses = clean_count(counters[3]["data-to-value"])
-    booster_shots = clean_count(counters[4]["data-to-value"])
-    immunocompromised_doses = clean_count(counters[5]["data-to-value"])
+        return pd.Series(
+            data={
+                "total_vaccinations": total_vaccinations,
+                "people_vaccinated": people_vaccinated,
+                "people_fully_vaccinated": people_fully_vaccinated,
+                "total_boosters": total_boosters,
+                "date": date,
+            }
+        )
 
-    people_vaccinated = first_doses + unique_doses
-    people_fully_vaccinated = second_doses + unique_doses
-    total_boosters = booster_shots + immunocompromised_doses
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "vaccine", "Johnson&Johnson, Moderna, Pfizer/BioNTech, Oxford/AstraZeneca")
 
-    date = localdate("America/Jamaica")
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return ds.pipe(self.pipe_vaccine).pipe(self.pipe_metadata)
 
-    return pd.Series(
-        data={
-            "total_vaccinations": total_vaccinations,
-            "people_vaccinated": people_vaccinated,
-            "people_fully_vaccinated": people_fully_vaccinated,
-            "total_boosters": total_boosters,
-            "date": date,
-        }
-    )
-
-
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Jamaica")
-
-
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Johnson&Johnson, Moderna, Pfizer/BioNTech, Oxford/AstraZeneca")
-
-
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source)
+    def export(self):
+        data = self.read().pipe(self.pipeline)
+        self.export_datafile(df=data, attach=True)
 
 
 def main():
-    source = "https://vaccination.moh.gov.jm/"
-    data = read(source).pipe(pipeline, source)
-    increment(
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        total_boosters=data["total_boosters"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"],
-    )
+    Jamaica().export()
