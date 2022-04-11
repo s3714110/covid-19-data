30f8fe3265b43e24d0a56149b202516f2a600290
Cam Appel <cameron@ourworldindata.org>
2022-04-11 15:10:19 +0000

    Update nicaragua.py

--- scripts/src/cowidev/testing/incremental/nicaragua.py
+++ scripts/src/cowidev/testing/incremental/nicaragua.py
@@ -10,7 +10,6 @@ from epiweeks import Week
 
 
 from cowidev.utils import get_soup, clean_count, clean_date
-from cowidev.utils.clean import extract_clean_date
 from cowidev.utils.utils import download_file_from_url
 from cowidev.testing import CountryTestBase
 
@@ -19,16 +18,15 @@ class Nicaragua(CountryTestBase):
     location: str = "Nicaragua"
     units: str = "tests performed"
     source_label: str = "Ministry of Health"
-    source_url: str = "http://www.minsa.gob.ni/index.php/repository/Descargas-MINSA/COVID-19/Boletines-Epidemiol%C3%B3gico/Boletines-2022/"
     _base_url: str = "http://www.minsa.gob.ni/index.php/repository/func-download"
-    source_url_ref: str = None
+    source_url_ref: str = "http://www.minsa.gob.ni/index.php/repository/Descargas-MINSA/COVID-19/Boletines-Epidemiol%C3%B3gico/Boletines-2022/"
     regex: dict = {
         "title": r"Boletín Epidemiológico de la Semana No. ",
     }
 
     def read(self) -> pd.DataFrame:
         """Read data from source"""
-        soup = get_soup(self.source_url)
+        soup = get_soup(self.source_url_ref)
         df = self._parse_data(soup)
         return df
 
@@ -39,22 +37,29 @@ class Nicaragua(CountryTestBase):
         # Parse count from pdf
         count = self._extract_text_from_url(link)
         # Get the date from week num
-        date = self._parse_date(link)
+        date = self._parse_date(soup)
+        df = pd.DataFrame(
+            {
+                "Date": [date],
+                "Cumulative total": [count],
+            }
+        )
+        return df
 
     def _get_download_url(self, soup: BeautifulSoup) -> str:
         source_url_ref = soup.find("a", text=re.compile(self.regex["title"]))["href"]
         if not source_url_ref:
             raise ValueError("Article not found, please update the script")
-        response = requests.get(self.source_url_ref, allow_redirects=True)
+        response = requests.get(source_url_ref, allow_redirects=True)
         text = response.content.decode("utf-8")
         result = re.search("func-download(.*)'}", text).group(1)
         link = f"{self._base_url}{result}"
         return link
 
-    def _extract_text_from_url(self) -> str:
+    def _extract_text_from_url(self, link) -> str:
         """Extracts text from pdf."""
         with tempfile.NamedTemporaryFile() as tmp:
-            download_file_from_url(self.link, tmp.name)
+            download_file_from_url(link, tmp.name)
             with open(tmp.name, "rb") as f:
                 text = extract_text(f).replace("\n", " ")
         count = clean_count(re.search("• Acumulado: (.*)  Recuperados", text).group(1))
@@ -62,7 +67,7 @@ class Nicaragua(CountryTestBase):
             raise ValueError("Count not found, please update the script")
         return count
 
-    def _parse_date(self, week_num) -> Iterator:
+    def _parse_date(self, soup: BeautifulSoup) -> Iterator:
         """parses the date from the week number."""
         week_num = int(
             re.search(
@@ -72,3 +77,18 @@ class Nicaragua(CountryTestBase):
         )
         date = Week(2022, week_num, system="iso").enddate()
         return clean_date(date)
+
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.pipe(self.pipe_metadata)
+            .pipe(self.pipe_merge_current)
+            .drop_duplicates(subset=["Cumulative total"], keep="first")
+        )
+
+    def export(self):
+        df = self.read().pipe(self.pipeline)
+        self.export_datafile(df)
+
+
+def main():
+    Nicaragua().export()
