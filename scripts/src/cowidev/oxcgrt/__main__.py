b6ce8563322024566c31c4fd82b03f4c4ba8e1aa
lucasrodes <lucasrodes@users.noreply.github.com>
2022-04-15 13:03:13 +0200

    data(oxcgrt): add data from https://github.com/OxCGRT/covid-policy-scratchpad

--- scripts/src/cowidev/oxcgrt/__main__.py
+++ scripts/src/cowidev/oxcgrt/__main__.py
@@ -7,13 +7,14 @@ from ._parser import _parse_args
 
 
 FILE_DS = os.path.join(PATHS.INTERNAL_INPUT_BSG_DIR, "latest.csv")
+FILE_DIFF_DS = os.path.join(PATHS.INTERNAL_INPUT_BSG_DIR, "latest-differentiated.csv")
 FILE_GRAPHER = os.path.join(PATHS.INTERNAL_GRAPHER_DIR, "COVID Government Response (OxBSG).csv")
 FILE_COUNTRY_STD = os.path.join(PATHS.INTERNAL_INPUT_BSG_DIR, "bsg_country_standardised.csv")
 
 
 def run_step(step: str):
     if step == "etl":
-        run_etl(FILE_DS)
+        run_etl(FILE_DS, FILE_DIFF_DS)
     elif step == "grapher-file":
         run_grapheriser(FILE_DS, FILE_COUNTRY_STD, FILE_GRAPHER)
     elif step == "grapher-db":
--- scripts/src/cowidev/oxcgrt/etl.py
+++ scripts/src/cowidev/oxcgrt/etl.py
@@ -4,9 +4,12 @@ import pandas as pd
 class OxCGRTETL:
     def __init__(self) -> None:
         self.source_url = "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
+        self.source_url_diff = "https://raw.githubusercontent.com/OxCGRT/covid-policy-scratchpad/master/differentiated_vaccination_policies/OxCGRT_differentiated.csv"
 
     def extract(self):
-        return pd.read_csv(self.source_url, low_memory=False)
+        df = pd.read_csv(self.source_url, low_memory=False)
+        df_diff = pd.read_csv(self.source_url_diff, low_memory=False)
+        return df, df_diff
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         return df
@@ -14,11 +17,12 @@ class OxCGRTETL:
     def load(self, df: pd.DataFrame, output_path: str):
         df.to_csv(output_path, index=False)
 
-    def run(self, output_path: str):
-        df = self.extract()
+    def run(self, output_path: str, output_path_diff: str):
+        df, df_diff = self.extract()
         self.load(df, output_path)
+        self.load(df_diff, output_path_diff)
 
 
-def run_etl(output_path: str):
+def run_etl(output_path: str, output_path_diff: str):
     etl = OxCGRTETL()
-    etl.run(output_path)
+    etl.run(output_path, output_path_diff)
