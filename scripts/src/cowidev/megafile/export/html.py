710179b80ef79975790b7588576a0c00a41ab590
lucasrodes <lucasrodes@users.noreply.github.com>
2022-04-26 10:50:14 +0200

    fix(megafile): types

--- scripts/src/cowidev/megafile/export/html.py
+++ scripts/src/cowidev/megafile/export/html.py
@@ -12,17 +12,20 @@ def pipe_vax_locations_to_html(df: pd.DataFrame) -> pd.DataFrame:
         "Palestine",
     }
     faq = ' (see <a href="https://ourworldindata.org/covid-vaccinations#frequently-asked-questions">FAQ</a>)'
+    codes = [i for i in df.iso_code.tolist() if "OWID_" not in i or i == "OWID_KOS"]
     df = df.assign(
         location=(df.location.apply(lambda x: f"<td><strong>{x}</strong>{faq if x in country_faqs else ''}</td>")),
         source=('<td><a href="' + df.source_website + '">' + df.source_name + "</a></td>"),
-        last_observation_date=(df.last_observation_date.apply(lambda x: f"<td>{x.strftime('%b. %e, %Y')}</td>")),
+        last_observation_date=(
+            pd.to_datetime(df.last_observation_date).apply(lambda x: f"<td>{x.strftime('%b. %e, %Y')}</td>")
+        ),
         vaccines=(df.vaccines.apply(lambda x: f"<td>{x}</td>")),
     )[["location", "source", "last_observation_date", "vaccines"]]
     df.columns = [col.capitalize().replace("_", " ") for col in df.columns]
     body = ("<tr>" + df.sum(axis=1) + "</tr>").sum(axis=0)
     header = "<tr>" + "".join(f"<th>{col}</th>" for col in df.columns) + "</tr>"
     html_table = f"<table><tbody>{header}{body}</tbody></table>"
-    coverage_info = f"Vaccination against COVID-19 has now started in {len(self._countries_covered)} locations."
+    coverage_info = f"Vaccination against COVID-19 has now started in {len(codes)} locations."
     html_table = (f'<p><strong>{coverage_info}</strong></p><div class="tableContainer">{html_table}</div>\n').replace(
         "  ", " "
     )
@@ -31,7 +34,8 @@ def pipe_vax_locations_to_html(df: pd.DataFrame) -> pd.DataFrame:
 
 def generate_htmls():
     # Vaccinations html source table
+    print(1)
     df = pd.read_csv(PATHS.DATA_VAX_META_FILE)
-    html_table = df.pipe(pipe_vax_locations_to_html)
+    html_table = pipe_vax_locations_to_html(df)
     with open(PATHS.DATA_INTERNAL_VAX_TABLE, "w") as f:
         f.write(html_table)
