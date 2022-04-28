f26fba614beffe98da5ddfe9d6bd32883b61a95e
lucasrodes <lucasrodes@users.noreply.github.com>
2022-04-29 00:26:25 +0200

    fix(cmd,megafile): missing arg

--- scripts/src/cowidev/cmd/megafile.py
+++ scripts/src/cowidev/cmd/megafile.py
@@ -9,6 +9,7 @@ def click_megafile(ctx):
     """COVID-19 data integration pipeline (former megafile)"""
     feedback_log(
         func=generate_megafile,
+        logger=ctx.obj["logger"],
         server=ctx.obj["server"],
         domain="Megafile",
         text_success="Public data files generated.",
