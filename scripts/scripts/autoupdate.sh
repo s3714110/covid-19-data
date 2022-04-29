464fdf426af21572c29939ee8fe71c17006831b4
lucasrodes <lucasrodes@users.noreply.github.com>
2022-04-29 15:06:04 +0200

    enhance(updates): reorganize

--- scripts/scripts/autoupdate.sh
+++ scripts/scripts/autoupdate.sh
@@ -47,17 +47,14 @@ git pull
 # JHU
 
 # Attempt to download JHU CSVs
-# run_python 'import jhu; jhu.download_csv()'
 cowid jhu get
+git_push "data(jhu): downloaded data from JHU"
 
-# If there are any unstaged changes in the repo, then one of
-# the CSVs has changed, and we need to run the update script.
 hour=$(date +%H)
 if [ $hour == 00 ] || [ $hour == 02 ] || [ $hour == 04 ] || [ $hour == 06 ] || [ $hour == 08 ] || [ $hour == 10 ] || [ $hour == 12 ] || [ $hour == 14 ] ||[ $hour == 16 ] || [ $hour == 18 ] || [ $hour == 20 ] || [ $hour == 22 ]; then
   if has_changed './scripts/input/jhu/*'; then
     echo "Generating JHU files..."
     cowid --server jhu generate
-    cowid --server megafile
     # python $SCRIPTS_DIR/scripts/jhu.py --skip-download
     git_push "data(jhu): automated update"
   else
@@ -65,15 +62,8 @@ if [ $hour == 00 ] || [ $hour == 02 ] || [ $hour == 04 ] || [ $hour == 06 ] || [
   fi
 fi
 
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid --server jhu grapher-db
-# run_python 'import jhu; jhu.update_db()'
-
 # =====================================================================
 # Decoupling charts
-
 hour=$(date +%H)
 if [ $hour == 01 ] ; then
   echo "Generating decoupling dataset..."
@@ -81,15 +71,8 @@ if [ $hour == 01 ] ; then
   git_push "data(decoupling): automated update"
 fi
 
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid --server decoupling grapher-db
-
 # =====================================================================
 # VAX ICER
-# This basically download the vaccination data needed for some countries
-# The idea is that here we put extremely slow scripts, so their updates are managed separately
 hour=$(date +%H)
 if [ $hour == 03 ] ; then
   echo "Generating ICE vaccination data..."
@@ -105,24 +88,17 @@ if [ $hour == 05 ] || [ $hour == 17 ] ; then
   echo "Generating hospital & ICU export..."
   cowid --server hosp generate
   cowid --server hosp grapher-io
-  cowid --server megafile
   git_push "data(hosp): automated update"
 fi
-
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid hosp grapher-db
 
 # =====================================================================
 # Vaccinations
 
 hour=$(date +%H)
 if [ $hour == 07 ] ; then
-  echo "Generating Vaccination (get, process, generate) & megafile..."
+  echo "Generating Vaccination (get, process, generate)..."
   cowid --server vax get
   cowid --server vax process generate
-  cowid --server megafile
   git_push "data(vax): automated update"
 fi
 
@@ -132,8 +108,7 @@ fi
 
 hour=$(date +%H)
 if [ $hour == 09 ] ; then
-
-  # Download CSV
+  # Generate dataset
   cowid --server gmobility generate
 
   echo "Generating Google Mobility export..."
@@ -142,21 +117,14 @@ if [ $hour == 09 ] ; then
   if has_changed './scripts/grapher/Google Mobility Trends (2020).csv'; then
     git_push "data(mobility): automated update"
   fi
-
 fi
-
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid --server gmobility grapher-db
 
 # =====================================================================
 # Swedish Public Health Agency
 
 # Attempt to download data
 cowid --server sweden get
-# If there are any unstaged changes in the repo, then one of
-# the CSVs has changed, and we need to run the update script.
+git_push "data(sweden): downloaded data for Sweden"
 hour=$(date +%H)
 if [ $hour == 11 ] ; then
   if has_changed './scripts/input/sweden/sweden_deaths_per_day.csv'; then
@@ -168,13 +136,8 @@ if [ $hour == 11 ] ; then
   fi
 fi
 
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid --server sweden grapher-db
 # =====================================================================
 # UK subnational data
-
 hour=$(date +%H)
 if [ $hour == 13 ] ; then
   # Download CSV
@@ -183,29 +146,8 @@ if [ $hour == 13 ] ; then
   git_push "data(uk): automated update"
 fi
 
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-cowid --server uk-nations grapher-db
 # =====================================================================
 # US vaccinations
-
-# # Attempt to download CDC data
-# run_python 'import us_vaccinations; us_vaccinations.download_data()'
-
-# # If there are any unstaged changes in the repo, then one of
-# # the CSVs has changed, and we need to run the update script.
-# echo "Generating US vaccination file..."
-# run_python 'import us_vaccinations; us_vaccinations.generate_dataset()'
-# if has_changed './public/data/vaccinations/us_state_vaccinations.csv'; then
-#   git add .
-#   git commit -m "Automated US vaccination update"
-#   git push
-#   run_python 'import us_vaccinations; us_vaccinations.update_db()'
-# else
-#   echo "US vaccination export is up to date"
-# fi
-
 hour=$(date +%H)
 if [ $hour == 15 ] ; then
   echo "Generating US vaccination files..."
@@ -219,36 +161,25 @@ fi
 
 # =====================================================================
 # Variants
-# If there are any unstaged changes in the repo, then one of
-# the CSVs has changed, and we need to run the update script.
 hour=$(date +%H)
 if [ $hour == 19 ] ; then
   echo "Generating CoVariants dataset..."
-  cowid variants generate
-  cowid variants grapher-io
+  cowid --server variants generate
+  cowid --server variants grapher-io
   git_push "data(variants): automated update"
 fi
 
-
 # =====================================================================
 # Excess Mortality
-# If there are any unstaged changes in the repo, then one of
-# the CSVs has changed, and we need to run the update script.
 hour=$(date +%H)
 if [ $hour == 21 ] ; then
   echo "Generating CoVariants dataset..."
-  cowid xm generate
-  cowid --server megafile
+  cowid --server xm generate
   git_push "data(xm): automated update"
 fi
 
 # =====================================================================
 # Policy responses
-
-# The policy update files change far too often (every hour or so).
-# We don't want to run an update if one has already been run in the
-# last 6 hours.
-
 OXCGRT_CSV_PATH=./scripts/input/bsg/latest.csv
 hour=$(date +%H)
 if [ $hour == 23 ] ; then
@@ -267,8 +198,8 @@ else
   echo "OxCGRT CSV was recently updated; skipping download"
 fi
 
-# Always run the database update.
-# The script itself contains a check against the database
-# to make sure it doesn't run unnecessarily.
-# run_python 'import oxcgrt; oxcgrt.update_db()'
-cowid --server oxcgrt grapher-db
+
+# =====================================================================
+# Megafile
+cowid --server megafile
+git_push "data(megafile): automated update"
