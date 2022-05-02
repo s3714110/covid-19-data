f43db3412f5aa50d2fd7c18983dee54d36c16730
lucasrodes <lucasrodes@users.noreply.github.com>
2022-05-02 11:33:23 +0200

    fix(vax,brb): regex

--- scripts/src/cowidev/vax/incremental/barbados.py
+++ scripts/src/cowidev/vax/incremental/barbados.py
@@ -14,7 +14,7 @@ class Barbados(CountryVaxBase):
     regex: dict = {
         "title": r"COVID-19 Update",
         "people_vaccinated": r"at least one dose is ([\d,]+)",
-        "people_fully_vaccinated": r"fully (?:vaccinated|vaccinated persons) is ([\d,]+)",
+        "people_fully_vaccinated": r"fully? (?:vaccinated|vaccinated persons) is ([\d,]+)",
         # "people_vaccinated": (
         #     r"(\d+) persons \((?:[\d.]+) per cent of the eligible population\) have received at least one dose"
         # ),
@@ -37,6 +37,7 @@ class Barbados(CountryVaxBase):
         if not link:
             raise ValueError("Article not found, please update the script")
         self.source_url_ref = link
+        # print(link)
         soup = get_soup(link)
         # Get the metrics
         metrics = self._parse_metrics(soup)
