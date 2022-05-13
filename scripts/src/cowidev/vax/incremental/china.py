74fd1cea099ce800f02142d2a787adfcb44c6d2f
Yuanyuan Wei <aywi@users.noreply.github.com>
2022-05-13 22:23:34 +0800

    fix(vax,chn): Fix regex for data on 2022-05-12

--- scripts/src/cowidev/vax/incremental/china.py
+++ scripts/src/cowidev/vax/incremental/china.py
@@ -19,20 +19,22 @@ class China(CountryVaxBase):
         "date": r"截至(20\d{2})年(\d{1,2})月(\d{1,2})日",
         "total_vaccinations": r"([\d\.]+\s*万)剂次",
     }
-    regex_complete = {
+    regex_complete: dict = {
         "title": r"国务院联防联控机制(20\d{2})年(\d{1,2})月(\d{1,2})日新闻发布会文字实录",
-        "summary": r"截至(\d{1,2})月(\d{1,2})日.*疫苗([\d\.亿零]+万)剂次.*全程接种的?人数(?:为|.{0,9}达到)([\d\.亿零]+万)人",
-        "vaccinated": r"(?:接种|疫苗)的?总人数(?:达到?|为)([\d\.亿零]+万)",
-        "boosters": r"加强免疫(?:已经)?接种的?是?([\d\.亿零]+万)人",
+        "summary": r"截至(\d{1,2})月(\d{1,2})日.*(?:疫苗|接种)([\d\.亿零]+万)剂",
+        "fully": r"全程接种的?人?数?(?:为|是|.{0,9}达到?)?([\d\.亿零]+万)",
+        "vaccinated": r"(?:接种|疫苗)的?总人数(?:达到?|为|是)([\d\.亿零]+万)",
+        "boosters": r"加强免疫已?经?接种的?是?([\d\.亿零]+万)",
     }
-    num_links_complete = 3
-    timeout = 30
+    num_links_complete: int = 3
+    timeout: int = 30
 
-    def read(self, last_update: str):
+    def read(self, last_update: str) -> pd.DataFrame:
         data = []
         options = sel_options(headless=True, firefox=True)
         options.set_capability("pageLoadStrategy", "none")
         with get_driver(options=options, firefox=True, timeout=self.timeout) as driver:
+            # Load the page until the list of links is loaded
             driver.get(self.source_url)
             Wait(driver, self.timeout).until(EC.presence_of_element_located((By.CLASS_NAME, "zxxx_list")))
             driver.execute_script("window.stop();")
@@ -45,27 +47,30 @@ class China(CountryVaxBase):
             assert data_["date"] <= last_update, "Only read data back to: " + data_["date"]
         return pd.DataFrame(data)
 
+    def _get_links(self, driver):
+        elems = driver.find_elements_by_css_selector(".zxxx_list>li>a")
+        return [elem.get_property("href") for elem in elems if self.regex["title"] in elem.text]
+
     def _parse_data(self, driver, url):
+        # Load the page until the end of the text is loaded
         driver.get(url)
         Wait(driver, self.timeout).until(EC.url_to_be(url))
         Wait(driver, self.timeout).until(EC.text_to_be_present_in_element((By.ID, "xw_box"), "万剂次"))
         driver.execute_script("window.stop();")
         elem = driver.find_element_by_id("xw_box")
+        # Apply regex and get metrics
         return {
             "date": extract_clean_date(elem.text, self.regex["date"], "%Y %m %d"),
-            "total_vaccinations": clean_count(re.search(self.regex["total_vaccinations"], elem.text).group(1)),
             "source_url": url,
+            "total_vaccinations": clean_count(re.search(self.regex["total_vaccinations"], elem.text).group(1)),
         }
 
-    def _get_links(self, driver) -> list:
-        elems = driver.find_elements_by_css_selector(".zxxx_list>li>a")
-        return [elem.get_property("href") for elem in elems if self.regex["title"] in elem.text]
-
-    def read_complete(self):
+    def read_complete(self) -> pd.DataFrame:
         records = []
         options = sel_options(headless=True, firefox=True)
         options.set_capability("pageLoadStrategy", "none")
         with get_driver(options=options, firefox=True, timeout=self.timeout) as driver:
+            # Load the page until the list of links is loaded
             driver.get(self.source_url_complete)
             Wait(driver, self.timeout).until(EC.presence_of_element_located((By.CLASS_NAME, "zxxx_list")))
             driver.execute_script("window.stop();")
@@ -88,6 +93,7 @@ class China(CountryVaxBase):
                 num += float(num_100m.group(1)) * 1e8
             return int(num)
 
+        # Load the page until the end of the text is loaded
         driver.get(url)
         Wait(driver, self.timeout).until(EC.url_to_be(url))
         Wait(driver, self.timeout).until(EC.text_to_be_present_in_element((By.ID, "xw_box"), "到此结束"))
@@ -96,27 +102,25 @@ class China(CountryVaxBase):
         # Apply regex
         year = re.search(self.regex_complete["title"], driver.title).group(1)
         summary = re.search(self.regex_complete["summary"], elem.text)
-        if summary:
-            month, day, total_vaccinations, people_fully_vaccinated = summary.groups()
+        fully = re.search(self.regex_complete["fully"], elem.text)
+        if summary and fully:
+            month, day, total_vaccinations = summary.groups()
         else:
             return
         vaccinated = re.search(self.regex_complete["vaccinated"], elem.text)
         boosters = re.search(self.regex_complete["boosters"], elem.text)
         # Get metrics
-        metrics = {
-            "total_vaccinations": _clean_count(total_vaccinations),
-            "people_vaccinated": _clean_count(vaccinated.group(1)) if vaccinated else None,
-            "people_fully_vaccinated": _clean_count(people_fully_vaccinated),
-            "total_boosters": _clean_count(boosters.group(1)) if boosters else None,
+        return {
             "date": clean_date(f"{year}-{month}-{day}", "%Y-%m-%d"),
             "source_url": url,
+            "total_vaccinations": _clean_count(total_vaccinations),
+            "people_vaccinated": _clean_count(vaccinated.group(1)) if vaccinated else None,
+            "people_fully_vaccinated": _clean_count(fully.group(1)),
+            "total_boosters": _clean_count(boosters.group(1)) if boosters else None,
         }
-        return metrics
 
     def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.assign(
-            location=self.location,
-        )
+        return df.assign(location=self.location)
 
     def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.assign(vaccine="CanSino, IMBCAMS, KCONVAC, Sinopharm/Beijing, Sinopharm/Wuhan, Sinovac, ZF2001")
