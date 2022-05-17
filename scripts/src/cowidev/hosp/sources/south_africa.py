316f0d1e6fbf894745220d4467dfb4964fbb4f23
lucasrodes <lucasrodes@users.noreply.github.com>
2022-05-17 14:42:54 +0200

    enhance(hosp,zaf): add data

--- scripts/src/cowidev/hosp/sources/south_africa.py
+++ scripts/src/cowidev/hosp/sources/south_africa.py
@@ -4,10 +4,14 @@ from cowidev.utils.clean import clean_date_series
 
 
 METADATA = {
-    "source_url_flow": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGCkIwakQ5rpfXky9FZhDwr3qUgerfhBLSzn9OsA79yQ_2G_y-_Ns9JjRJZWXD5kxJ3qicoL7bHGjE/pub?gid=1044172863&single=true&output=csv",
+    "source_url_flow": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGCkIwakQ5rpfXky9FZhDwr3qUgerfhBLSzn9OsA79yQ_2G_y-_Ns9JjRJZWXD5kxJ3qicoL7bHGjE/pub?gid=1044172863&output=csv",
     "source_url_stock": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGCkIwakQ5rpfXky9FZhDwr3qUgerfhBLSzn9OsA79yQ_2G_y-_Ns9JjRJZWXD5kxJ3qicoL7bHGjE/pub?gid=1317252294&single=true&output=csv",
-    "source_url_ref": "https://docs.google.com/spreadsheets/d/1Ln3FO3E0hz2r_kCCWM6on9Wd18wRvrkiIV-bbE2EDQ0/edit#gid=1044172863",
-    "source_name": "National Policy Data Observatory (NPDO) at the Council for Scientific and Industrial Research (CSIR)",
+    "source_url_ref": (
+        "https://docs.google.com/spreadsheets/d/1Ln3FO3E0hz2r_kCCWM6on9Wd18wRvrkiIV-bbE2EDQ0/edit#gid=1044172863"
+    ),
+    "source_name": (
+        "National Policy Data Observatory (NPDO) at the Council for Scientific and Industrial Research (CSIR)"
+    ),
     "entity": "South Africa",
 }
 
