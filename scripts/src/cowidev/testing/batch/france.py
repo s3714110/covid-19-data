9c0f18f748232ee520d374b9b1d161dca71d3389
Cam Appel <cameron@ourworldindata.org>
2022-05-19 17:23:58 +0100

    fix(france)

--- scripts/src/cowidev/testing/batch/france.py
+++ scripts/src/cowidev/testing/batch/france.py
@@ -12,28 +12,18 @@ class France(CountryTestBase):
     source_label = "National Public Health Agency"
 
     def read(self):
-        df_ppl = self._read_ppl_tested()
-        df_pr = self._read_pr()
-        df = pd.merge(df_ppl, df_pr, on="Date", how="outer").sort_values("Date")
-        return df
-
-    def _read_ppl_tested(self):
-        url = "https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-4503-930a-7b08dc0adc7c"
-        df = pd.read_csv(url, sep=";", usecols=["jour", "cl_age90", "T"])
+        url = "https://www.data.gouv.fr/fr/datasets/r/d349accb-56ef-4b53-b218-46c2a7f902e0"
+        df = pd.read_csv(url, sep=";", usecols=["jour", "cl_age90", "T", "P"])
+        df["T"] = [int(x.replace(",00", "")) for x in df["T"]]
+        df["P"] = [int(x.replace(",00", "")) for x in df["P"]]
         df = (
-            df[df.cl_age90 == 0]
-            .rename(columns={"jour": "Date", "T": "Daily change in cumulative total"})
-            .drop(columns=["cl_age90"])
+            df.rename(columns={"jour": "Date"})
+            .groupby("Date", as_index=False)
+            .agg(**{"Daily change in cumulative total": ("T", sum), "pos": ("P", sum)})
         )
-        return df
-
-    def _read_pr(self):
-        df = pd.read_csv(
-            "https://www.data.gouv.fr/fr/datasets/r/381a9472-ce83-407d-9a64-1b8c23af83df",
-            usecols=["extract_date", "tx_pos"],
-        )
-        df.loc[:, "tx_pos"] = df["tx_pos"].div(100).round(3)
-        df = df.rename(columns={"extract_date": "Date", "tx_pos": "Positive rate"})
+        df["Positive rate"] = (
+            (df["pos"].rolling(7).mean()) / (df["Daily change in cumulative total"].rolling(7).mean())
+        ).round(3)
         return df
 
     def pipeline(self, df: pd.DataFrame):
