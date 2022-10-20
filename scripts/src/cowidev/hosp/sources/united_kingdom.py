5f2f30e746cd191928cb3dbe895cab6a02dcf3e0
lucasrodes <lucasrodes@users.noreply.github.com>
2022-10-20 19:17:34 +0200

    enhance(hosp,uk): use uk api library

--- scripts/src/cowidev/hosp/sources/united_kingdom.py
+++ scripts/src/cowidev/hosp/sources/united_kingdom.py
@@ -1,36 +1,40 @@
-import pandas as pd
-from cowidev.utils.web.download import read_csv_from_url
+from uk_covid19 import Cov19API
 
 
 METADATA = {
-    "source_url": "https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=hospitalCases&metric=newAdmissions&metric=covidOccupiedMVBeds&format=csv",
+    "source_url": "https://coronavirus.data.gov.uk/details/download",
     "source_url_ref": "https://coronavirus.data.gov.uk/details/healthcare",
     "source_name": "Government of the United Kingdom",
     "entity": "United Kingdom",
 }
 
 
+def read():
+    metrics = {
+        "date": "date",
+        "Daily hospital occupancy": "hospitalCases",
+        "Weekly new hospital admissions": "newAdmissions",
+        "Daily ICU occupancy": "covidOccupiedMVBeds",
+    }
+    api = Cov19API(
+        filters=["areaType=overview"],
+        structure=metrics,
+    )
+    df = api.get_dataframe()
+    return df
+
+
 def main():
-    df = read_csv_from_url(
-        url=METADATA["source_url"],
-        usecols=["date", "hospitalCases", "newAdmissions", "covidOccupiedMVBeds"],
-    )
-
+    # Read
+    df = read()
+    # Sort rows
     df = df.sort_values("date")
-
-    df["newAdmissions"] = df.newAdmissions.rolling(7).sum()
-
+    # Smooth metric
+    df["Weekly new hospital admissions"] = df["Weekly new hospital admissions"].rolling(7).sum()
+    # Unpivot
     df = df.melt("date", var_name="indicator").dropna(subset=["value"])
-    df["indicator"] = df.indicator.replace(
-        {
-            "hospitalCases": "Daily hospital occupancy",
-            "covidOccupiedMVBeds": "Daily ICU occupancy",
-            "newAdmissions": "Weekly new hospital admissions",
-        }
-    )
-
+    # Metadata
     df["entity"] = METADATA["entity"]
-
     return df, METADATA
 
 
