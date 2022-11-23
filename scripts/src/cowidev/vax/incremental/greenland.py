005092bce239e93c6a8d04532ed2455220ecd534
lucasrodes <lucasrodes@users.noreply.github.com>
2022-11-23 12:28:20 +0100

    fix(vax,grl): change in source layout

--- scripts/src/cowidev/vax/incremental/greenland.py
+++ scripts/src/cowidev/vax/incremental/greenland.py
@@ -10,7 +10,7 @@ from cowidev.vax.utils.incremental import enrich_data, increment
 
 class Greenland:
     location: str = "Greenland"
-    source_url: str = "https://corona.nun.gl"
+    source_url: str = "https://corona.nun.gl/en/"
     regex = {"date": r".*Nutarterneqarpoq: (\d+. [a-zA-Z]+202\d)"}
 
     def read(self) -> pd.Series:
@@ -22,23 +22,26 @@ class Greenland:
         return {**self._parse_data_metrics(soup), **self._parse_data_date(soup)}
 
     def _parse_data_metrics(self, soup) -> dict:
-        counters = soup.find_all(class_="text-brand-blue")
-        dose_1 = clean_count(re.search(r"Innuttaasut ([\d\.]+)", counters[1].parent.find_all("dd")[-1].text).group(1))
-        dose_2 = clean_count(re.search(r"Innuttaasut ([\d\.]+)", counters[2].parent.find_all("dd")[-1].text).group(1))
-        if dose_1 < dose_2:
-            raise ValueError("dose_1 cannot be higher than dose_2")
+        # Find raw elements with metric values
+        regex_dose1 = (
+            r"flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r"
+        )
+        regex_dose2 = r"flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l"
+        counter_1 = soup.find_all(class_=regex_dose1)
+        counter_2 = soup.find_all(class_=regex_dose2)
+        assert len(counter_1) == len(counter_2) == 1
+        counter_1, counter_2 = counter_1[0], counter_2[0]
+        # Extract metric falues
+        dose_1 = clean_count(re.search(r".* ([\d\.]*) citizens", counter_1.text).group(1))
+        dose_2 = clean_count(re.search(r".* ([\d\.]*) citizens", counter_2.text).group(1))
         return {"people_vaccinated": dose_1, "people_fully_vaccinated": dose_2}
 
     def _parse_data_date(self, soup) -> dict:
-        date_raw = soup.find(class_="text-gray-500").text
-        date = extract_clean_date(
-            date_raw.strip() + str(datetime.now().year), self.regex["date"], "%d. %B%Y", lang="en"
-        )
-        if date > localdate("America/Havana", force_today=True):
-            date = extract_clean_date(
-                date_raw.strip() + str(datetime.now().year - 1), self.regex["date"], "%d. %B%Y", lang="en"
-            )
-
+        regex = r"mt-4 sm:mt-6 text-sm text-center text-gray-500"
+        counter_date = soup.find_all(class_=regex)
+        assert len(counter_date) == 1
+        counter_date = counter_date[0]
+        date = extract_clean_date(counter_date.text, r".* Updated (\d*\. [A-Z][a-z]*)", "%d. %B", replace_year=2022)
         return {"date": date}
 
     def pipe_source(self, ds: pd.Series) -> pd.Series:
@@ -59,6 +62,7 @@ class Greenland:
 
     def export(self):
         data = self.read().pipe(self.pipeline)
+        print(data)
         increment(
             location=data["location"],
             total_vaccinations=data["total_vaccinations"],
