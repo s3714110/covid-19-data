236a89321bd4f17e3d6eceab76e0e71bb0b6d93d
lucasrodes <lucasrodes@users.noreply.github.com>
2022-12-05 14:13:54 +0100

    fix(vax,ncy): collect metrics

--- scripts/src/cowidev/vax/incremental/northern_cyprus.py
+++ scripts/src/cowidev/vax/incremental/northern_cyprus.py
@@ -6,6 +6,7 @@ import pandas as pd
 from cowidev.utils.clean import clean_date
 from cowidev.utils.web.scraping import get_soup
 from cowidev.vax.utils.incremental import enrich_data, increment
+from cowidev.utils.clean.dates import localdate
 
 
 def read(source: str) -> pd.Series:
@@ -20,12 +21,13 @@ def parse_data(soup: BeautifulSoup) -> pd.Series:
     date = re.search(r"[\d\.]{10}", soup.find(class_="counter").text).group(0)
     date = clean_date(date, "%d.%m.%Y")
 
+    print(numbers)
     return pd.Series(
         data={
-            "total_vaccinations": int(numbers[0]["data-count"]),
-            "people_vaccinated": int(numbers[1]["data-count"]),
-            "people_fully_vaccinated": int(numbers[2]["data-count"]),
-            "total_boosters": int(numbers[3]["data-count"]),
+            "total_vaccinations": int(numbers[-1]["data-count"]),
+            "people_vaccinated": int(numbers[0]["data-count"]),
+            "people_fully_vaccinated": int(numbers[1]["data-count"]),
+            "total_boosters": int(numbers[2]["data-count"]) + int(numbers[3]["data-count"]),
             "date": date,
         }
     )
@@ -43,8 +45,26 @@ def enrich_source(ds: pd.Series, source: str) -> pd.Series:
     return enrich_data(ds, "source_url", source)
 
 
+def pipe_fix_boosters(ds: pd.Series) -> pd.Series:
+    if ds["total_boosters"] == 0:
+        ds["total_boosters"] = None
+    return ds
+
+
+def pipe_fix_date(ds: pd.Series) -> pd.Series:
+    if ds["date"] < localdate("Asia/Nicosia", minus_days=15):
+        ds["date"] = localdate("Asia/Nicosia")
+    return ds
+
+
 def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source)
+    return (
+        ds.pipe(enrich_location)
+        .pipe(enrich_vaccine)
+        .pipe(enrich_source, source)
+        .pipe(pipe_fix_boosters)
+        .pipe(pipe_fix_date)
+    )
 
 
 def main():
